2692cead8825d29e8978ce043392a7d3a6d907b4
karnaankit <40463873+karnaankit@users.noreply.github.com>
2022-10-14 14:59:40 +0545

    Update find_topic.py

--- topics/find_topic.py
+++ topics/find_topic.py
@@ -1,27 +1,18 @@
 import csv
 from operator import itemgetter
 from test_package import ngrams
+from collections import defaultdict
 
-topics = set()
-count = dict()
+topics = list()
+count_bigram = defaultdict(int)
+count_trigram = defaultdict(int)
 file = open("urls.csv")
-file2 = open("note.txt", 'w')
 csvreader = csv.reader(file)
 for row in csvreader:
-    for i in range(0, len(row)):
-        if i is 5:
-            bigram = ngrams.generate_ngrams(row[i], 2)
-            for item1 in bigram:
-                file2.write(item1 + "\n")
-            trigram = ngrams.generate_ngrams(row[i], 3)
-            for item2 in trigram:
-                file2.write(item2 + "\n")
-file2.close()
-file1 = open("note.txt", 'r')
-for item in file1:
-    item = item.rstrip()
-    if item not in count:
-        count[item] = 1
-    else:
-        count[item] += 1
-file1.close()
+        bigram = ngrams.generate_ngrams(row[5], 2)
+        for item1 in bigram:
+            count_bigram[item1] += 1
+        trigram = ngrams.generate_ngrams(row[5], 3)
+        for item2 in trigram:
+            count_trigram[item2] += 1
+file.close()
