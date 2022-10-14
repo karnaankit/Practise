fdd1648156d7eef4e2e7f7bffedd52b5e2d010a1
karnaankit <40463873+karnaankit@users.noreply.github.com>
2022-10-14 15:34:11 +0545

    Update find_topic.py

--- topics/find_topic.py
+++ topics/find_topic.py
@@ -6,13 +6,18 @@ from collections import defaultdict
 topics = list()
 count_bigram = defaultdict(int)
 count_trigram = defaultdict(int)
-file = open("urls.csv")
-csvreader = csv.reader(file)
-for row in csvreader:
-        bigram = ngrams.generate_ngrams(row[5], 2)
-        for item1 in bigram:
-            count_bigram[item1] += 1
-        trigram = ngrams.generate_ngrams(row[5], 3)
-        for item2 in trigram:
-            count_trigram[item2] += 1
-file.close()
+with open("urls.csv") as file:
+    csvreader = csv.reader(file)
+    for row in csvreader:
+            bigram = ngrams.generate_ngrams(row[5], 2)
+            for item1 in bigram:
+                count_bigram[item1] += 1
+            trigram = ngrams.generate_ngrams(row[5], 3)
+            for item2 in trigram:
+                count_trigram[item2] += 1
+top_20_bigram = dict(sorted(count_bigram.items(), key=itemgetter(1), reverse=True)[:20])
+top_20_trigram = dict(sorted(count_trigram.items(), key=itemgetter(1), reverse=True)[:20])
+with open('top_20_bigram.txt', "w") as f:
+    print(top_20_bigram, file=f)
+with open('top_20_trigram.txt', "w") as f1:
+    print(top_20_trigram, file=f1)
