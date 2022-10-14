7627f3466ac344220a3622e0d2d72be9f534d760
karnaankit <40463873+karnaankit@users.noreply.github.com>
2022-10-14 18:01:10 +0545

    Update find_topic.py

--- topics/find_topic.py
+++ topics/find_topic.py
@@ -1,10 +1,11 @@
 import csv
 import json
+import pandas as pd
 from operator import itemgetter
 from test_package import ngrams
 from collections import defaultdict
 
-topics = list()
+topics = dict()
 count_bigram = defaultdict(int)
 count_trigram = defaultdict(int)
 with open("urls.csv") as file:
@@ -24,3 +25,20 @@ with open('top_20_bigram.txt', "w") as f:
     f.write(json.dumps(top_20_bigram, indent=4))
 with open('top_20_trigram.txt', "w") as f1:
     f1.write(json.dumps(top_20_trigram, indent=4))
+
+
+with open('urls.csv') as file:
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        data = row[5]
+        data = data.split('--')[0]
+        bigram = ngrams.generate_ngrams(data, 2)
+        for item1 in bigram:
+            if item1 in top_20_bigram:
+                topics[row[5]] = item1
+        trigram = ngrams.generate_ngrams(data, 3)
+        for item2 in trigram:
+            if item2 in top_20_trigram:
+                topics[row[5]] = item2
+df = pd.DataFrame.from_dict(topics, orient='index')
+df.to_csv('topics.csv')
