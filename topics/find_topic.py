b04196bb98b72951c97d5078025df70b6de90ef2
karnaankit <40463873+karnaankit@users.noreply.github.com>
2022-10-15 08:24:50 +0545

    Add files via upload

--- topics/find_topic.py
+++ topics/find_topic.py
@@ -4,10 +4,13 @@ import pandas as pd
 from operator import itemgetter
 from test_package import ngrams
 from collections import defaultdict
-from difflib import SequenceMatcher
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
+          'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+          'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska',
+          'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+          'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas',
+          'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
 count_bigram = defaultdict(int)
 count_trigram = defaultdict(int)
@@ -22,8 +25,17 @@ with open("urls.csv") as file:
         trigram = ngrams.generate_ngrams(data, 3)
         for item2 in trigram:
             count_trigram[item2] += 1
-top_20_bigram = dict(sorted(count_bigram.items(), key=itemgetter(1), reverse=True)[:20])
-top_20_trigram = dict(sorted(count_trigram.items(), key=itemgetter(1), reverse=True)[:20])
+sorted_bigram = sorted(count_bigram.items(), key=itemgetter(1), reverse=True)
+sorted_trigram = sorted(count_trigram.items(), key=itemgetter(1), reverse=True)
+for state in STATES:
+    for item in sorted_bigram:
+        if state in item[0]:
+            sorted_bigram.remove(item)
+    for item in sorted_trigram:
+        if state in item[0]:
+            sorted_trigram.remove(item)
+top_20_bigram = dict(sorted_bigram[:20])
+top_20_trigram = dict(sorted_trigram[:20])
 with open('top_20_bigram.txt', "w") as f:
     f.write(json.dumps(top_20_bigram, indent=4))
 with open('top_20_trigram.txt', "w") as f1:
@@ -34,30 +46,18 @@ with open('urls.csv') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
         flag = 0
-        flag1 = 0
         data = row[5]
         data = data.split('--')[0]
         trigram = ngrams.generate_ngrams(data, 3)
         for item2 in trigram:
-            for state in STATES:
-                if similar(state, item2) > 0.5:
-                    flag1 = 1
-            if flag1 is 1:
-                continue
             if item2 in top_20_trigram:
                 flag = 1
                 if item2 in topics[row[5]]:
                     continue
                 topics[row[5]].append(item2)
         bigram = ngrams.generate_ngrams(data, 2)
-        flag1 = 0
         for item1 in bigram:
             if flag is 1:
-                continue
-            for state in STATES:
-                if similar(state, item1) > 0.5:
-                    flag1 = 1
-            if flag1 is 1:
                 continue
             if item1 in top_20_bigram:
                 if item1 in topics[row[5]]:
