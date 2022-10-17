5353b0d1e402b5495f34189778b8d079bfabeccb
karnaankit <karna.ankit1@gmail.com>
2022-10-17 11:07:42 +0545

    regex

--- daraz/scrape.py
+++ daraz/scrape.py
@@ -2,7 +2,9 @@ import requests
 import json
 import pandas as pd
 import csv
+import re
 search = 'nuts'
+pattern1 = re.compile(r'(\d+)(\s*)(kg|Kg|KG|gm|Gm|G|g)')
 
 
 def get_prices(search_title):
@@ -19,26 +21,30 @@ def get_prices(search_title):
 
 
 def get_units(file):
-    df = pd.DataFrame(columns=['Title', 'Units', 'Price'])
+    def normalize_units(data):
+        quantity = re.compile(r'(\d+)')
+        uni = re.compile(r'(\d+)(\s*)(gm|Gm|G|g)')
+        if re.match(uni, data):
+            size = int(quantity.search(data).group(1))/1000
+            data = str(size) + 'Kg'
+            return data
+        else:
+            return data
+    df = pd.DataFrame(columns=['Title', 'Units', 'Normalized unit', 'Price'])
     with open(file) as f:
         csvreader = csv.reader(f)
         for row in csvreader:
             if row[0] == 'Title':
                 continue
-            title = row[0]
+            unit = pattern1.search(row[0]).group(0)
+            normalized = normalize_units(unit)
+            name = re.sub(pattern1, '', row[0])
+            if name.split('-')[0] is '':
+                name = name.split('-')[1]
+            else:
+                name = name.split('-')[0]
             price = row[1]
-            ind = 0
-            for char in title:
-                if char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                    name = title[:ind]
-                    unit = title[ind:]
-                    for char1 in unit:
-                        if char1 in ['g', 'G']:
-                            unit = unit[:unit.index(char1) + 1]
-                            break
-                    break
-                ind += 1
-            df = df.append({'Title': name, 'Units': unit, 'Price': price}, ignore_index=True)
+            df = df.append({'Title': name, 'Units': unit, 'Normalized unit': normalized, 'Price': price}, ignore_index=True)
     df.to_csv('unit.csv', index=False)
 
 
