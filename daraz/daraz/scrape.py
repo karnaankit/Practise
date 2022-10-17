e78338f55bd932879647260f5c0d532a2130d6f6
karnaankit <karna.ankit1@gmail.com>
2022-10-17 16:16:43 +0545

    update

--- daraz/daraz/scrape.py
+++ daraz/daraz/scrape.py
@@ -3,55 +3,74 @@ import json
 import pandas as pd
 import csv
 import re
-search = 'nuts'
-pattern1 = re.compile(r'(\d+)(\s*)(kg|Kg|KG|gm|Gm|G|g)')
+import os
 
 
-def get_prices(search_title):
-    df = pd.DataFrame(columns=['Title', 'Price'])
-    search_url = 'https://www.daraz.com.np/catalog/?q=' + search_title
-    response = requests.get(search_url)
-    response = response.text.split('window.pageData=')[1]
-    data = json.loads(response.split('</script>')[0])
-    for item in data['mods']['listItems']:
-        title = item['name']
-        price = item['price']
-        df = df.append({'Title': title, 'Price': price}, ignore_index=True)
-    df.to_csv('title.csv', index=False)
+def scrape(search_term):
+    def get_prices(search_title):
+        df = pd.DataFrame(columns=['Title', 'Price'])
+        search_url = 'https://www.daraz.com.np/catalog/?q=' + search_title
+        response = requests.get(search_url)
+        response = response.text.split('window.pageData=')[1]
+        data = json.loads(response.split('</script>')[0])
+        for item in data['mods']['listItems']:
+            title = item['name']
+            price = item['price']
+            df = df.append({'Title': title, 'Price': price}, ignore_index=True)
+        df.to_csv('title.csv', index=False)
 
+    def get_name_unit(row1):
+        amount = re.compile(r'(\d+)')
+        pattern1 = re.compile(r'(\d+)(\s*)(kg|Kg|KG|gm|Gm|G|g)')
+        result = list()
+        result.append(pattern1.search(row1).group(0))
+        temp = normalize_units(result[0])
+        if re.match(amount, temp):
+            result.append(amount.search(temp).group(1))
+            result.append(re.sub(amount, '', temp))
+        result.append(re.sub(pattern1, '', row1).strip('-').split('-')[0])
+        return result
 
-def get_name_unit(row1):
-    result = list()
-    result.append(pattern1.search(row1).group(0))
-    result.append(normalize_units(result[0]))
-    result.append(re.sub(pattern1, '', row1).strip('-').split('-')[0])
-    return result
+    def normalize_units(data):
+        quantity = re.compile(r'(\d+)')
+        uni = re.compile(r'(\d+)(\s*)(kg|Kg|KG|kG)')
+        if re.match(uni, data):
+            size = int(quantity.search(data).group(1)) * 1000
+            data = str(size) + 'gm'
+            return data
+        else:
+            return data
 
+    def get_units(file):
+        df = pd.DataFrame(columns=['Title', 'Units', 'Amount', 'Normalized unit', 'Price'])
+        with open(file) as f:
+            csvreader = csv.reader(f)
+            for row in csvreader:
+                if row[0] == 'Title':
+                    continue
+                items = get_name_unit(row[0])
+                price = row[1]
+                df = df.append({'Title': items[3], 'Units': items[0], 'Amount': items[1], 'Normalized unit': items[2],
+                                'Price': price}, ignore_index=True)
+        df.to_csv(os.getcwd()+'/data/'+search_term+'.csv', index=False)
 
-def normalize_units(data):
-    quantity = re.compile(r'(\d+)')
-    uni = re.compile(r'(\d+)(\s*)(gm|Gm|G|g)')
-    if re.match(uni, data):
-        size = int(quantity.search(data).group(1))/1000
-        data = str(size) + 'Kg'
-        return data
-    else:
-        return data
-
-
-def get_units(file):
-    df = pd.DataFrame(columns=['Title', 'Units', 'Normalized unit', 'Price'])
-    with open(file) as f:
-        csvreader = csv.reader(f)
-        for row in csvreader:
-            if row[0] == 'Title':
-                continue
-            items = get_name_unit(row[0])
-            price = row[1]
-            df = df.append({'Title': items[2], 'Units': items[0], 'Normalized unit': items[1], 'Price': price}, ignore_index=True)
-    df.to_csv('unit.csv', index=False)
+    get_prices(search_term)
+    get_units('title.csv')
+    os.remove('title.csv')
 
 
 if __name__ == '__main__':
-    get_prices(search)
-    get_units('title.csv')
+    search = input("Enter search term")
+    path = os.getcwd()+'/data'
+    df1 = pd.DataFrame(columns=['Title', 'Units', 'Amount', 'Normalized unit', 'Price'])
+    if search+'.csv' in os.listdir(path):
+        with open(path+'/'+search+'.csv') as f:
+            csvreader = csv.reader(f)
+            for row in csvreader:
+                if row[0] == 'Title':
+                    continue
+                df1 = df1.append({'Title': row[0], 'Units': row[1], 'Amount': row[2], 'Normalized unit': row[3],
+                                'Price': row[4]}, ignore_index=True)
+            print(df1)
+    else:
+        scrape(search)
